77c66fba29610094f15d4cb9e26346c511110ac3
Danylo Khrystenko <D.Khrystenko@sms.ed.ac.uk>
2026-04-27 00:45:40 +0100

    Starting Template

---
+++ src/main/java/danylo/mod/DecorModDataGenerator.java
@@ -0,0 +1,11 @@
+package danylo.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DecorModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
